d1b6c02dde3dc33b315efe1d4a48308b01fb0066
Maxime CHAGNOLLEAU <maxime.chagnolleau@consertotech.pro>
2025-01-14 17:16:40 +0100

    fix phpstan > session type

--- sources/lib/Converter/ConverterPooler.php
+++ sources/lib/Converter/ConverterPooler.php
@@ -6,6 +6,7 @@ use PommProject\Foundation\Converter\ConverterClient;
 use PommProject\Foundation\Converter\ConverterHolder;
 use PommProject\Foundation\Converter\ConverterPooler as BaseConverterPooler;
 use PommProject\Foundation\Exception\ConverterException;
+use PommProject\ModelManager\Session;
 
 class ConverterPooler extends BaseConverterPooler
 {
@@ -20,7 +21,9 @@ class ConverterPooler extends BaseConverterPooler
         if (!$this->converterHolder->hasType($identifier)) {
             // try to intialize it with the matching model
             try {
-                $this->getSession()->getModel($identifier . 'Model');
+                /** @var Session $session */
+                $session = $this->getSession();
+                $session->getModel($identifier . 'Model');
             } catch (\Exception) {
                 throw new ConverterException(sprintf("No converter registered for type '%s'.", $identifier));
             }
